f4d707a895026fcc0f9ee50b15220ec3485b04df
Ankur <ankur@theindierise.com>
2026-06-02 17:08:31 +0530

    test commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/sonner";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({subsets:['latin'],variable:'--font-inter'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +28,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${inter.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+          <Toaster/>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
